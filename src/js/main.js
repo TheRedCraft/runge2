93f6a35b9e6ab8589192c031381f02940efbad3a
theredcraft <bananeananas29@gmail.com>
2023-05-11 18:22:43 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -2,20 +2,20 @@ let darkmode = false;
 let html_cotact = `
 	<div style="position: absolute; top: 0; display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100%; width: 100%;">
 	<input
-		style="width: 60%; margin: 1rem; height: 5rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: #c2c2c2"
+		style="width: 35rem; margin: 0.5rem; height: 3rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: #c2c2c2"
 		placeholder="Name"
 		type="text"
 		class="in1" /><input
-		style="width: 60%; margin: 1rem; height: 5rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: #c2c2c2"
+		style="width: 35rem; margin: 0.5rem; height: 3rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: #c2c2c2"
 		placeholder="Email"
 		type="text"
 		class="in1" /><input
-		style="width: 60%; margin: 1rem; height: 13rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: #c2c2c2"
+		style="width: 35rem; margin: 0.5rem; height: 10rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: #c2c2c2"
 		placeholder="Message"
 		type="text"
 		class="in1" /><input
 		value="SEND"
-		style="width: 60%; margin: 1rem; height: 5rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: var(--token-c3cb0cd5-b51f-4513-b6f3-4eb1d93cac0a);"
+		style="width: 35rem; margin: 0.5rem; height: 3rem; border-radius: 15px; padding: 5px; background-color: var(--token-15111fde-133a-4f2a-a639-023410d025db); border: none; color: var(--token-c3cb0cd5-b51f-4513-b6f3-4eb1d93cac0a);"
 		type="button"
 		class="but1" />
 	</div>
@@ -30,4 +30,4 @@ setInterval(function () {
 		document.querySelector('.framer-13hlmmu').innerHTML = html_cotact;
 		document.querySelector('.Contact-container').innerHTML = ' ';
 	}
-}, 1000);
+}, 10);
